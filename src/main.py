efb76aa478f49b64fd41529628183e26cbe59aa4
a.zaynagabdinov <The.scorpion2006@yandex.ru>
2026-08-04 00:50:42 +0500

    refactor: main.py refactoring

--- src/main.py
+++ src/main.py
@@ -2,20 +2,8 @@ import argparse
 from src.service import BenchmarkService
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(prog='http-bench-service',
-                                     description='Тестирование доступности HTTP-серверов',
-                                     epilog="Пример: python bench.py -H https://ya.ru -C 5"
-                                     )
-    parser.add_argument('-C', '--count', type=int, help='Количество запросов', default=1)
-    parser.add_argument('-O', '--output', type=str, help='Файл для сохранения результптов')
-
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-H', '--hosts', type=str, help='Хосты через запятую')
-    group.add_argument('-F', '--file', type=str, help='Файл со списком хостов')
-
-    args = parser.parse_args()
-
+def main():
+    args = parse_args()
     benchmark = BenchmarkService()
 
     try:
@@ -33,5 +21,20 @@ def parse_args():
         print(f"❌ Ошибка: {e}")
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(prog='http-bench-service',
+                                     description='Тестирование доступности HTTP-серверов',
+                                     epilog="Пример: python bench.py -H https://ya.ru -C 5"
+                                     )
+    parser.add_argument('-C', '--count', type=int, help='Количество запросов', default=1)
+    parser.add_argument('-O', '--output', type=str, help='Файл для сохранения результптов')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-H', '--hosts', type=str, help='Хосты через запятую')
+    group.add_argument('-F', '--file', type=str, help='Файл со списком хостов')
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    parse_args()
+    main()
